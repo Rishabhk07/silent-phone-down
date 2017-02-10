341b02515280278b30de455d6268da53cb3e1add
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-02-11 00:56:13 +0530

    crash fixed
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/silentflip/hptouchsmart/phonedown/MainActivity.java
+++ app/src/main/java/com/silentflip/hptouchsmart/phonedown/MainActivity.java
@@ -1,5 +1,6 @@
 package com.silentflip.hptouchsmart.phonedown;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -38,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+//        toolbar.setTitleTextColor(Color.WHITE);
 
                 drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
                 actionBarDrawerToggle = new ActionBarDrawerToggle(this , drawerLayout  , toolbar , R.string.drawer_open , R.string.drawer_close);
+//                actionBarDrawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
                 navigationView = (NavigationView) findViewById(R.id.navgation_view);
 
 
